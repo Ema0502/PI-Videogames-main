a5d0ef2e053a33d9bfaf78850b7216851e953764
Ema0502 <ema050296@gmail.com>
2023-05-16 15:35:54 -0300

    searchbar terminado

--- PI-Videogames-main/client/src/components/SearchBar/SearchBar.jsx
+++ PI-Videogames-main/client/src/components/SearchBar/SearchBar.jsx
@@ -13,20 +13,28 @@ const SearchBar = () => {
 
   //búsqueda en la API
   const onSearch = async () => {
+    // try {
+    //   const { data } = await axios.get(`https://api.rawg.io/api/games?key=ae685cb2d21b48f9bd71da0064c1b6d5&search=${name}`);
+    //   // Actualizar el estado con los resultados de la API
+    //   setResults(data.results);
+    // } catch (error) {
+    //   // Mostrar una alerta en caso de error
+    //   alert("No se ha encontrado la información solicitada");
+    // }
+
     try {
-      const { data } = await axios.get(
-        `https://api.rawg.io/api/games?key=ae685cb2d21b48f9bd71da0064c1b6d5&search=${name}`
-      );
+      const { data } = await axios.get(`http://localhost:3001/search?name=${name}`);
       // Actualizar el estado con los resultados de la API
-      setResults(data.results);
+      setResults(data);
     } catch (error) {
       // Mostrar una alerta en caso de error
       alert("No se ha encontrado la información solicitada");
     }
+
   };
   
   return (
-    <div className={ style.container }>
+    <div className={style.container}>
       <input
         type="search"
         className={style.SearchBar}
@@ -44,13 +52,24 @@ const SearchBar = () => {
           {/* Iterar sobre los resultados y renderizar cada uno */}
           {results.map((result) => (
             <div key={result.id} className={style.box}>
-              {/* Mostrar el nombre del videojuego */}
               <h1>{result.name}</h1>
+              <img
+                src={result.background_image}
+                alt=""
+                className={style.image}
+              />
               {/* Mostrar los géneros del videojuego separados por coma */}
-              <p>{result.genres.map((genre) => genre.name).join(", ")}</p>
+              <p>
+                Generos: {result.genres.map((genre) => genre.name).join(", ")}
+              </p>
             </div>
           ))}
         </div>
+      )}
+      {results.length === 0 && (
+        <p className={style.box}>
+          Puedes utilizar el buscador para buscar tu juego favorito
+        </p>
       )}
     </div>
   );
